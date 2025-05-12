88d40dd6d5789b4fa90ec5b42998787355b9afa7
Adnan <skadnan40605@gmail.com>
2025-05-12 16:07:20 +0530

    Add additional deployment configuration files to fix Nixpacks build error

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Exit on error
+set -o errexit
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Create static directory if it doesn't exist
+mkdir -p static
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Run migrations
+python manage.py migrate 
